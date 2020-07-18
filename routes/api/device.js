d770ad2511cb67bc1f14d4a11893495760dd276c
ssingh <sandy.boon@gmail.com>
2020-07-18 07:55:37 -0400

    added authentication to device routes and added findAll route

--- routes/api/device.js
+++ routes/api/device.js
@@ -1,8 +1,13 @@
 const deviceRouter = require('express').Router();
 const device = require('../../controllers/deviceController');
+const isShopAuthenticated = require('../../config/middleware/isAuthenticatedShop');
+
+deviceRouter.use(isShopAuthenticated);
 
 deviceRouter.post('/', device.create);
 
 deviceRouter.get('/', device.find);
+
+deviceRouter.get('/all', device.findAll);
 
 module.exports = deviceRouter;
